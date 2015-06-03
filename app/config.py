b117c44ff0c94cfa69ed02e67bc1020434214d51
Jason White <jason.white@shopify.com>
2015-06-03 00:31:26 -0400

    Revert "move config to app folder, fix pylinter in circle"
    This reverts commit 2756396821903020f9fca199dc8306a37c87aac3.

--- app/config.py
+++ app/config.py
@@ -6,6 +6,6 @@ import yaml
 def config(key):
     filename = path.join(path.dirname(path.dirname(path.abspath(__file__))), 'config.yml')
     with open(filename, 'r') as config_file:
-        yaml_file = yaml.load(config_file)
-        result = yaml_file[key]
+        file = yaml.load(config_file)
+        result = file[key]
     return result
