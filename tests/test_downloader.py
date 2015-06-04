fe01a7982c4c529d3645a23b9e4bf4c4e10d8731
Jason White <jason.white@shopify.com>
2015-06-04 01:49:39 -0400

    centralizing database access, added downloader module

---
+++ tests/test_downloader.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+
+
+class TestDownloader(object):
+    def test_act_file_location(self):
+        assert False
+
+    def test_act_file_location_with_unicode(self):
+        assert False
